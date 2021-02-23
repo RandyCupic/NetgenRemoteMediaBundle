daa9d90bfab08a7bf3be03ff64fb7b046c144efd
Randy Čupić <randy@netgen.hr>
2021-02-23 22:32:24 +0100

    Adjust field type class

--- bundle/Core/FieldType/RemoteMedia/Type.php
+++ bundle/Core/FieldType/RemoteMedia/Type.php
@@ -4,32 +4,21 @@ declare(strict_types=1);
 
 namespace Netgen\Bundle\RemoteMediaBundle\Core\FieldType\RemoteMedia;
 
+use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
 use eZ\Publish\Core\Base\Exceptions\InvalidArgumentType;
 use eZ\Publish\Core\FieldType\FieldType;
 use eZ\Publish\Core\FieldType\Value as BaseValue;
 use eZ\Publish\SPI\FieldType\Value as SPIValue;
 use eZ\Publish\SPI\Persistence\Content\FieldValue;
 
-class Type extends FieldType
+final class Type extends FieldType
 {
-    /**
-     * Returns the field type identifier for this field type.
-     *
-     * @return string
-     */
-    public function getFieldTypeIdentifier()
+    public function getFieldTypeIdentifier(): string
     {
         return 'ngremotemedia';
     }
 
-    /**
-     * Returns a human readable string representation from the given $value
-     * It will be used to generate content name and url alias if current field
-     * is designated to be used in the content name/urlAlias pattern.
-     *
-     * @return int
-     */
-    public function getName(SPIValue $value)
+    public function getName(SPIValue $value, FieldDefinition $fieldDefinition, string $languageCode): string
     {
         if (!empty($value->resourceId)) {
             return $value->resourceId;
@@ -39,23 +28,17 @@ class Type extends FieldType
     }
 
     /**
-     * Returns the empty value for this field type.
-     *
-     * @return Value
+     * @return \Netgen\Bundle\RemoteMediaBundle\Core\FieldType\RemoteMedia\Value
      */
-    public function getEmptyValue()
+    public function getEmptyValue(): Value
     {
         return new Value();
     }
 
     /**
-     * Converts an $hash to the Value defined by the field type.
-     *
-     * @param mixed $hash
-     *
      * @return \Netgen\Bundle\RemoteMediaBundle\Core\FieldType\RemoteMedia\Value
      */
-    public function fromHash($hash)
+    public function fromHash($hash): Value
     {
         if (!\is_array($hash)) {
             return $this->getEmptyValue();
@@ -74,24 +57,12 @@ class Type extends FieldType
         return new Value($hash);
     }
 
-    /**
-     * Converts the given $value into a plain hash format.
-     *
-     * @param \Netgen\Bundle\RemoteMediaBundle\Core\FieldType\RemoteMedia\Value|\eZ\Publish\SPI\FieldType\Value $value
-     *
-     * @return array
-     */
-    public function toHash(SPIValue $value)
+    public function toHash(SPIValue $value): array
     {
         return (array) $value;
     }
 
-    /**
-     * Converts a $value to a persistence value.
-     *
-     * @return \eZ\Publish\SPI\Persistence\Content\FieldValue
-     */
-    public function toPersistenceValue(SPIValue $value)
+    public function toPersistenceValue(SPIValue $value): FieldValue
     {
         if ($value instanceof InputValue) {
             return new FieldValue(
@@ -118,11 +89,9 @@ class Type extends FieldType
     }
 
     /**
-     * Converts a persistence $fieldValue to a Value.
-     *
      * @return \Netgen\Bundle\RemoteMediaBundle\Core\FieldType\RemoteMedia\Value
      */
-    public function fromPersistenceValue(FieldValue $fieldValue)
+    public function fromPersistenceValue(FieldValue $fieldValue): Value
     {
         if ($fieldValue->data === null) {
             return $this->getEmptyValue();
@@ -137,14 +106,7 @@ class Type extends FieldType
         return new Value($fieldValue->data);
     }
 
-    /**
-     * Returns if the given $value is considered empty by the field type.
-     *
-     * @param \eZ\Publish\Core\FieldType\Value $value
-     *
-     * @return bool
-     */
-    public function isEmptyValue(SPIValue $value)
+    public function isEmptyValue(SPIValue $value): bool
     {
         if ($value instanceof InputValue && !empty($value->input_uri)) {
             return false;
@@ -153,33 +115,16 @@ class Type extends FieldType
         return $value === null || $value === $this->getEmptyValue() || empty($value->resourceId);
     }
 
-    /**
-     * Returns whether the field type is searchable.
-     *
-     * @return bool
-     */
-    public function isSearchable()
+    public function isSearchable(): bool
     {
         return true;
     }
 
-    /**
-     * Returns information for FieldValue->$sortKey relevant to the field type.
-     *
-     * @return bool
-     */
     protected function getSortInfo(BaseValue $value)
     {
-        return false;
+        return null;
     }
 
-    /**
-     * Inspects given $inputValue and potentially converts it into a dedicated value object.
-     *
-     * @param mixed $inputValue
-     *
-     * @return Value $value the potentially converted input value
-     */
     protected function createValueFromInput($inputValue)
     {
         if ($inputValue instanceof InputValue) {
@@ -196,12 +141,7 @@ class Type extends FieldType
         return $inputValue;
     }
 
-    /**
-     * Throws an exception if value structure is not of expected format.
-     *
-     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException if the value does not match the expected structure
-     */
-    protected function checkValueStructure(BaseValue $value)
+    protected function checkValueStructure(BaseValue $value): void
     {
         if ($value instanceof Value && !\is_string($value->resourceId)) {
             throw new InvalidArgumentType('$value', 'string', $value->resourceId);
@@ -210,14 +150,7 @@ class Type extends FieldType
         }
     }
 
-    /**
-     * Throws an exception if the given $value is not an instance of the supported value subtype.
-     *
-     * @param mixed $value a value returned by {@see createValueFromInput()}
-     *
-     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException if the parameter is not an instance of the supported value subtype
-     */
-    protected static function checkValueType($value)
+    protected static function checkValueType($value): void
     {
         if (!$value instanceof Value && !$value instanceof InputValue) {
             throw new InvalidArgumentType('$value', 'Netgen\\Bundle\\RemoteMediaBundle\\Core\\FieldType\\RemoteMedia\\Value or "Netgen\\Bundle\\RemoteMediaBundle\\Core\\FieldType\\RemoteMedia\\InputValue",', $value);
